ca53b4ab6b7c45bd66c82d8ca98fdb17198e120f
cn-src <public@javaer.cn>
2021-11-15 13:50:28 +0800

    fix: AllOrNoneEmptyValidator

--- jany-validation/src/main/java/cn/javaer/jany/validation/AllOrNoneEmptyValidator.java
+++ jany-validation/src/main/java/cn/javaer/jany/validation/AllOrNoneEmptyValidator.java
@@ -1,8 +1,8 @@
 package cn.javaer.jany.validation;
 
-import cn.hutool.core.bean.DynaBean;
 import cn.hutool.core.util.ArrayUtil;
 import cn.hutool.core.util.ObjectUtil;
+import cn.hutool.core.util.ReflectUtil;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
@@ -25,11 +25,9 @@ public class AllOrNoneEmptyValidator implements ConstraintValidator<AllOrNoneEmp
         if (ArrayUtil.isEmpty(fields)) {
             return true;
         }
-        // TODO 性能？
-        DynaBean bean = DynaBean.create(value);
         int i = 0;
         for (String field : fields) {
-            if (ObjectUtil.isEmpty(bean.get(field))) {
+            if (ObjectUtil.isEmpty(ReflectUtil.getFieldValue(value, field))) {
                 i++;
             }
         }
