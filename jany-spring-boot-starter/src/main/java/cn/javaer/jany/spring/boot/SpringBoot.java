a367bb15b666f554b92b9399bb64dcf331d40d9e
cn-src <public@javaer.cn>
2021-11-15 23:38:13 +0800

    update SpringBootAdminLocal

--- jany-spring-boot-starter/src/main/java/cn/javaer/jany/spring/boot/SpringBoot.java
+++ jany-spring-boot-starter/src/main/java/cn/javaer/jany/spring/boot/SpringBoot.java
@@ -1,11 +1,16 @@
 package cn.javaer.jany.spring.boot;
 
+import cn.hutool.core.net.NetUtil;
 import cn.javaer.jany.util.IoUtils;
 import cn.javaer.jany.util.ReflectionUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.util.LinkedHashSet;
 import java.util.Properties;
 
 /**
@@ -24,6 +29,22 @@ public class SpringBoot {
                 final InputStream adminInput = SpringBoot.class
                     .getResourceAsStream("/default-boot-admin-local.properties");
                 final Properties adminProps = IoUtils.readProperties(adminInput);
+                final LinkedHashSet<InetAddress> addresses = NetUtil.localAddressList(address ->
+                    !address.isLoopbackAddress() && address instanceof Inet4Address);
+
+                InetAddress used = null;
+                try {
+                    for (InetAddress address : addresses) {
+                        if (address.isSiteLocalAddress() && address.isReachable(1)) {
+                            used = address;
+                        }
+                    }
+                }
+                catch (IOException ignore) {
+                }
+                if (null != used) {
+                    adminProps.put("server.address", used.getHostAddress());
+                }
                 props.putAll(adminProps);
             });
         app.setDefaultProperties(props);
