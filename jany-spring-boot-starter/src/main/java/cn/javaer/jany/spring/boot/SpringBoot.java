ac66ce018bf951fbf09f5a45545989eb80d2e2df
cn-src <public@javaer.cn>
2021-11-15 21:27:44 +0800

    update SpringBootAdminLocal

--- jany-spring-boot-starter/src/main/java/cn/javaer/jany/spring/boot/SpringBoot.java
+++ jany-spring-boot-starter/src/main/java/cn/javaer/jany/spring/boot/SpringBoot.java
@@ -1,10 +1,12 @@
 package cn.javaer.jany.spring.boot;
 
 import cn.javaer.jany.util.IoUtils;
+import cn.javaer.jany.util.ReflectionUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 import java.io.InputStream;
+import java.util.Properties;
 
 /**
  * @author cn-src
@@ -13,8 +15,18 @@ public class SpringBoot {
     public static ConfigurableApplicationContext run(final Class<?> primarySource,
                                                      final String... args) {
         final SpringApplication app = new SpringApplication(primarySource);
+
         final InputStream input = SpringBoot.class.getResourceAsStream("/default-boot.properties");
-        app.setDefaultProperties(IoUtils.readProperties(input));
+        final Properties props = IoUtils.readProperties(input);
+
+        ReflectionUtils.getClass("de.codecentric.boot.admin.server.config.EnableAdminServer")
+            .ifPresent(aClass -> {
+                final InputStream adminInput = SpringBoot.class
+                    .getResourceAsStream("/default-boot-admin-local.properties");
+                final Properties adminProps = IoUtils.readProperties(adminInput);
+                props.putAll(adminProps);
+            });
+        app.setDefaultProperties(props);
         return app.run(args);
     }
 }
