f436b3ed0af172992b286c680cdaac373d2832c1
cn-src <public@javaer.cn>
2021-11-17 23:52:09 +0800

    update ErrorInfoController

--- jany-spring/src/main/java/cn/javaer/jany/spring/web/exception/ErrorInfoController.java
+++ jany-spring/src/main/java/cn/javaer/jany/spring/web/exception/ErrorInfoController.java
@@ -1,16 +1,19 @@
 package cn.javaer.jany.spring.web.exception;
 
+import cn.javaer.jany.util.IoUtils;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.MissingResourceException;
-import java.util.ResourceBundle;
+import java.util.Properties;
 
 /**
  * @author cn-src
@@ -20,25 +23,24 @@ import java.util.ResourceBundle;
 @RequestMapping
 public class ErrorInfoController implements InitializingBean {
 
-    private Map<String, String> errorsMap;
+    private Map<Object, Object> errorsMap;
 
     @Operation(summary = "错误码表")
     @GetMapping("error_infos")
-    public Map<String, String> errorInfos() {
+    public Map<Object, Object> errorInfos() {
         return errorsMap;
     }
 
     private void load(String resourceBundle) {
-        try {
-            ResourceBundle defaultErrorsMessages = ResourceBundle.getBundle(resourceBundle);
-            for (String error : defaultErrorsMessages.keySet()) {
-                if (error.startsWith("RUNTIME_")) {
-                    continue;
-                }
-                errorsMap.put(error, ErrorMessageSource.getMessage(error));
+        final ClassPathResource resource = new ClassPathResource(resourceBundle);
+        if (resource.exists()) {
+            try {
+                Properties props = IoUtils.readProperties(resource.getInputStream());
+                errorsMap.putAll(props);
             }
-        }
-        catch (final MissingResourceException ignore) {
+            catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
         }
     }
 
