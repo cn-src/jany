522eff19a914cca3e9111fe710f0bfc721b9d56c
cn-src <public@javaer.cn>
2021-11-25 23:37:09 +0800

    feat: minio

--- jany-spring-boot-starter-minio/src/test/java/cn/javaer/jany/spring/autoconfigure/minio/MinioAutoConfigurationTest.java
+++ jany-spring-boot-starter-minio/src/test/java/cn/javaer/jany/spring/autoconfigure/minio/MinioAutoConfigurationTest.java
@@ -1,12 +1,15 @@
 package cn.javaer.jany.spring.autoconfigure.minio;
 
+import cn.javaer.jany.spring.autoconfigure.minio.empty.EmptyConfig;
 import cn.javaer.jany.spring.autoconfigure.minio.test.MinioTest;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ContextConfiguration;
 
 /**
  * @author cn-src
  */
 @MinioTest
+@ContextConfiguration(classes = {EmptyConfig.class})
 class MinioAutoConfigurationTest {
 
     private MinioService minioService;
---
+++ jany-spring-boot-starter-minio/src/test/java/cn/javaer/jany/spring/autoconfigure/minio/empty/EmptyConfig.java
@@ -0,0 +1,7 @@
+package cn.javaer.jany.spring.autoconfigure.minio.empty;
+
+/**
+ * @author cn-src
+ */
+public class EmptyConfig {
+}
