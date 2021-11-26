fffcce3b998ebb9e6e2b8715a26ff12fc2a286ee
cn-src <public@javaer.cn>
2021-11-26 12:54:34 +0800

    feat: minio

--- jany-spring-boot-starter-minio/build.gradle.kts
+++ jany-spring-boot-starter-minio/build.gradle.kts
@@ -6,7 +6,7 @@ val minioVersion: String by project
 dependencies {
     api("io.minio:minio:$minioVersion")
 
-    optionalApi("org.springframework.boot:spring-boot-starter")
+    optionalApi("org.springframework.boot:spring-boot-starter-web")
     optionalApi("org.springframework.boot:spring-boot-starter-test")
     testImplementation("com.playtika.testcontainers:embedded-minio")
 }
