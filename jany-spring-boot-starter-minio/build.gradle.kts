6ab7d3490a9175d4eb0066fd3ba9bea3fc6a5cd4
cn-src <public@javaer.cn>
2021-12-16 14:34:13 +0800

    test: fix test

--- jany-spring-boot-starter-minio/build.gradle.kts
+++ jany-spring-boot-starter-minio/build.gradle.kts
@@ -8,5 +8,6 @@ dependencies {
 
     optionalApi("org.springframework.boot:spring-boot-starter-web")
     optionalApi("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.cloud:spring-cloud-starter-bootstrap:3.1.0")
     testImplementation("com.playtika.testcontainers:embedded-minio")
 }
