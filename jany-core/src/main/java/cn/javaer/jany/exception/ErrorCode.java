aac54860ae569147d76fb54eae9da87cd9f74d94
cn-src <public@javaer.cn>
2021-12-17 11:26:29 +0800

    feat: ErrorCode add code

--- jany-core/src/main/java/cn/javaer/jany/exception/ErrorCode.java
+++ jany-core/src/main/java/cn/javaer/jany/exception/ErrorCode.java
@@ -16,4 +16,21 @@ public @interface ErrorCode {
     String error();
 
     int status() default 500;
+
+    // ---- 40x
+
+    String BAD_REQUEST = "BAD_REQUEST";
+    String UNAUTHORIZED = "UNAUTHORIZED";
+    String FORBIDDEN = "FORBIDDEN";
+    String NOT_FOUND = "NOT_FOUND";
+
+    // ---- 50x
+
+    String INTERNAL_SERVER_ERROR = "INTERNAL_SERVER_ERROR";
+
+    // ---- 登录
+
+    String LOGIN_ERROR = "LOGIN_ERROR";
+    String LOGIN_ERROR_BAD_CREDENTIALS = "LOGIN_ERROR_BAD_CREDENTIALS";
+    String LOGIN_ERROR_DISABLED = "LOGIN_ERROR_DISABLED";
 }
