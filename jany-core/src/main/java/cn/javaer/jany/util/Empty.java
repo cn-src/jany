2caf7d22fc7bc586c410fe60891bb1988a92bb40
cn-src <public@javaer.cn>
2021-12-17 20:25:34 +0800

    重构: WebContext

--- jany-core/src/main/java/cn/javaer/jany/util/Empty.java
+++ jany-core/src/main/java/cn/javaer/jany/util/Empty.java
@@ -1,15 +1,17 @@
 package cn.javaer.jany.util;
 
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 /**
  * @author cn-src
  */
 public interface Empty {
     @SuppressWarnings("rawtypes") Function FUNCTION = o -> null;
+    @SuppressWarnings("rawtypes") Supplier SUPPLIER = () -> null;
 
     /**
-     * empty function.
+     * empty Function.
      *
      * @param <T> the type parameter
      * @param <R> the type parameter
@@ -19,5 +21,17 @@ public interface Empty {
     @SuppressWarnings("unchecked")
     static <T, R> Function<T, R> function() {
         return FUNCTION;
+    }
+
+    /**
+     * empty Supplier.
+     *
+     * @param <T> the type parameter
+     *
+     * @return the supplier
+     */
+    @SuppressWarnings("unchecked")
+    static <T> Supplier<T> supplier() {
+        return SUPPLIER;
     }
 }
--- jany-spring/src/main/java/cn/javaer/jany/spring/web/WebContext.java
+++ jany-spring/src/main/java/cn/javaer/jany/spring/web/WebContext.java
@@ -1,12 +1,25 @@
 package cn.javaer.jany.spring.web;
 
+import cn.javaer.jany.util.Empty;
+import cn.javaer.jany.util.ReflectionUtils;
 import com.yomahub.tlog.context.TLogContext;
+
+import java.util.function.Supplier;
 
 /**
  * @author cn-src
  */
-public interface WebContext {
+public class WebContext {
+
     String REQUEST_ID_PARAM = "requestId";
+
+    private static Supplier<String> requestIdFun = Empty.SUPPLIER;
+
+    static {
+        ReflectionUtils.getClass("com.yomahub.tlog.context.TLogContext").ifPresent(aClass -> {
+            requestIdFun = TLogContext::getTraceId;
+        });
+    }
 
     /**
      * 回去请求 id.
@@ -14,6 +27,6 @@ public interface WebContext {
      * @return the request id
      */
     static String getRequestId() {
-        return TLogContext.getTraceId();
+        return requestIdFun.get();
     }
 }
