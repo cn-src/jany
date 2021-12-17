ec8367f3788811a0adc621b5a549aa95b8b4c4b2
cn-src <public@javaer.cn>
2021-12-17 20:38:05 +0800

    重构: WebContext

--- jany-spring-boot-starter/src/main/java/cn/javaer/jany/spring/autoconfigure/web/exception/ErrorView.java
+++ jany-spring-boot-starter/src/main/java/cn/javaer/jany/spring/autoconfigure/web/exception/ErrorView.java
@@ -1,6 +1,6 @@
 package cn.javaer.jany.spring.autoconfigure.web.exception;
 
-import cn.javaer.jany.spring.web.WebContext;
+import cn.javaer.jany.exception.RuntimeErrorInfo;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.jetbrains.annotations.NotNull;
@@ -37,7 +37,7 @@ class ErrorView implements View {
         final Object timestamp = model.get("timestamp");
         final Object message = model.get("message");
         final Object trace = model.get("trace");
-        final Object requestId = model.get(WebContext.REQUEST_ID_PARAM);
+        final Object requestId = model.get(RuntimeErrorInfo.Fields.requestId);
         if (response.getContentType() == null) {
             response.setContentType(this.getContentType());
         }
