fb6156f100e3ba1c3180af51ca38c3d339236c9e
cn-src <public@javaer.cn>
2021-12-21 02:18:00 +0800

    build: update

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
-    `groovy-gradle-plugin`
+    `kotlin-dsl`
 }
 
 dependencies {
     implementation("com.github.ben-manes:gradle-versions-plugin:0.39.0")
+    implementation("io.spring.gradle:dependency-management-plugin:1.0.11.RELEASE")
 }
 
 repositories {
