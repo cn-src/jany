616c70ae1df6c8da6044ab5a5a6ce35addaa3952
cn-src <public@javaer.cn>
2022-05-28 20:37:44 +0800

    feat: add RequestUtils

---
+++ jany-spring/src/main/java/cn/javaer/jany/spring/util/RequestUtils.java
@@ -0,0 +1,30 @@
+package cn.javaer.jany.spring.util;
+
+import cn.hutool.core.io.FileUtil;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * @author cn-src
+ */
+public interface RequestUtils {
+    /**
+     * 判断文件是否是 Excel 文件。
+     *
+     * @param file MultipartFile
+     *
+     * @return true 表示是 Excel 文件。
+     */
+    static boolean isExcel(final MultipartFile file) {
+
+        if (null == file || file.isEmpty()) {
+            return false;
+        }
+        final String suffix = FileUtil.getSuffix(file.getOriginalFilename());
+        if (null == suffix) {
+            return false;
+        }
+        return suffix.equalsIgnoreCase("xls") ||
+            suffix.equalsIgnoreCase("xlsx") ||
+            suffix.equalsIgnoreCase("xlsm");
+    }
+}
