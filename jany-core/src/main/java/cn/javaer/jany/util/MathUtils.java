a0eae22abf74d933c7690c5c1be2373595b9b9d0
cn-src <public@javaer.cn>
2022-06-10 20:35:11 +0800

    docs: update javadoc

--- jany-core/src/main/java/cn/javaer/jany/util/MathUtils.java
+++ jany-core/src/main/java/cn/javaer/jany/util/MathUtils.java
@@ -1,17 +1,19 @@
 package cn.javaer.jany.util;
 
 /**
+ * 用于进行数学运算的工具类。
+ *
  * @author cn-src
  */
 public interface MathUtils {
 
     /**
-     * 包装类型取最小值，null 为最大.
+     * 返回 a 和 b 的最小值，null 值最大。
      *
-     * @param a the a
-     * @param b the b
+     * @param a 要比较的第一个数字。
+     * @param b 要与 a 进行比较的值。
      *
-     * @return the long
+     * @return 两个值中的最小值。
      */
     static Long min(Long a, Long b) {
         if (a == null) {
