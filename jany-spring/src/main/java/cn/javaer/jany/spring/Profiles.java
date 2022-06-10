dcdc55b5c92d8961da8bc5a0b3c5fc4593f63e90
cn-src <public@javaer.cn>
2022-06-10 21:07:13 +0800

    feat: add Profiles

---
+++ jany-spring/src/main/java/cn/javaer/jany/spring/Profiles.java
@@ -0,0 +1,29 @@
+package cn.javaer.jany.spring;
+
+/**
+ * Spring Profile 定义。
+ *
+ * @author cn-src
+ */
+public interface Profiles {
+
+    /**
+     * 默认环境。
+     */
+    String DEFAULT = "default";
+
+    /**
+     * 开发环境。
+     */
+    String DEV = "dev";
+
+    /**
+     * 测试环境。
+     */
+    String TEST = "test";
+
+    /**
+     * 生产环境。
+     */
+    String PROD = "prod";
+}
