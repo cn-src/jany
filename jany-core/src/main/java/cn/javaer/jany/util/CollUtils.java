5c4a3a9bfc1b9e01447f61bd23d1859c72ecfca7
cn-src <public@javaer.cn>
2022-06-27 23:02:53 +0800

    refactor: 以 hutool 重构工具类

---
+++ jany-core/src/main/java/cn/javaer/jany/util/CollUtils.java
@@ -0,0 +1,28 @@
+package cn.javaer.jany.util;
+
+import cn.hutool.core.collection.CollUtil;
+
+import java.util.Collection;
+
+/**
+ * @author cn-src
+ */
+public class CollUtils extends CollUtil {
+
+    /**
+     * 如果对象是一个集合，则返回它，否则抛出异常。
+     *
+     * @param obj 要转换为集合的对象。
+     *
+     * @return 对象的集合。
+     */
+    public static Collection<?> asCollection(Object obj) {
+        if (obj == null) {
+            return null;
+        }
+        if (obj instanceof Collection) {
+            return (Collection<?>) obj;
+        }
+        throw new IllegalArgumentException("unsupported type: " + obj.getClass());
+    }
+}
