a322acbcc6591bfad307ad504716df43b7044b67
cn-src <public@javaer.cn>
2022-06-27 23:15:37 +0800

    refactor: 以 hutool 重构工具类

--- jany-ebean/src/main/java/cn/javaer/jany/ebean/expression/Operator.java
+++ jany-ebean/src/main/java/cn/javaer/jany/ebean/expression/Operator.java
@@ -1,7 +1,9 @@
 package cn.javaer.jany.ebean.expression;
 
 import cn.hutool.core.util.ObjectUtil;
-import cn.javaer.jany.util.ObjUtils;
+import cn.javaer.jany.util.ArrayUtils;
+import cn.javaer.jany.util.CollUtils;
+import cn.javaer.jany.util.StrUtils;
 import io.ebean.ExpressionFactory;
 
 /**
@@ -19,9 +21,9 @@ public enum Operator {
         return factory.eq(property, value);
     }),
     arrayContains((factory, property, value) ->
-        factory.arrayContains(property, ObjUtils.toObjectArray(value))),
+        factory.arrayContains(property, ArrayUtils.toObjectArray(value))),
     arrayNotContains((factory, property, value) ->
-        factory.arrayNotContains(property, ObjUtils.toObjectArray(value))),
+        factory.arrayNotContains(property, ArrayUtils.toObjectArray(value))),
     eq(ExpressionFactory::eq),
 
     ne(ExpressionFactory::ne),
@@ -42,25 +44,25 @@ public enum Operator {
     lt(ExpressionFactory::lt),
     le(ExpressionFactory::le),
     like((factory, property, value) ->
-        factory.like(property, ObjUtils.castString(value))),
+        factory.like(property, StrUtils.cast(value))),
     ilike((factory, property, value) ->
-        factory.ilike(property, ObjUtils.castString(value))),
+        factory.ilike(property, StrUtils.cast(value))),
     startsWith((factory, property, value) ->
-        factory.startsWith(property, ObjUtils.castString(value))),
+        factory.startsWith(property, StrUtils.cast(value))),
     istartsWith((factory, property, value) ->
-        factory.istartsWith(property, ObjUtils.castString(value))),
+        factory.istartsWith(property, StrUtils.cast(value))),
     endsWith((factory, property, value) ->
-        factory.endsWith(property, ObjUtils.castString(value))),
+        factory.endsWith(property, StrUtils.cast(value))),
     iendsWith((factory, property, value) ->
-        factory.iendsWith(property, ObjUtils.castString(value))),
+        factory.iendsWith(property, StrUtils.cast(value))),
     contains((factory, property, value) ->
-        factory.contains(property, ObjUtils.castString(value))),
+        factory.contains(property, StrUtils.cast(value))),
     icontains((factory, property, value) ->
-        factory.icontains(property, ObjUtils.castString(value))),
+        factory.icontains(property, StrUtils.cast(value))),
     in((factory, property, value) ->
-        factory.in(property, ObjUtils.toCollection(value))),
+        factory.in(property, CollUtils.cast(value))),
     notIn((factory, property, value) ->
-        factory.notIn(property, ObjUtils.toCollection(value)));
+        factory.notIn(property, CollUtils.cast(value)));
 
     private final WhereExpression.ExprFunction fun;
 
