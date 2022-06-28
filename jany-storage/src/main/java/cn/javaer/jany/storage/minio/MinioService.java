b72ce42f1420e45e327873c9123af2fe989d72fd
cn-src <public@javaer.cn>
2022-06-28 11:22:03 +0800

    docs: update javadoc

--- jany-storage/src/main/java/cn/javaer/jany/storage/minio/MinioService.java
+++ jany-storage/src/main/java/cn/javaer/jany/storage/minio/MinioService.java
@@ -38,21 +38,56 @@ public interface MinioService {
     String presignedDeleteUrl(String objectName);
 
     /**
-     * Create presigned object.
+     * 它返回指定对象的预签名 URL
      *
-     * @param objectName the object name
+     * @param objectName 要预签名的对象的名称。
      *
-     * @return the presigned object
+     * @return 预签名的对象。
      */
     PresignedObject presignedObject(String objectName);
 
+    /**
+     * 将输入流的内容上传到具有给定名称的对象。
+     *
+     * 函数返回一个StorableObject对象，这个对象是一个Java对象，代表刚刚上传的对象
+     *
+     * @param objectName 要上传的对象的名称。
+     * @param in 要上传的文件的输入流。
+     *
+     * @return 可存储对象
+     */
     StorableObject upload(String objectName, InputStream in);
 
+    /**
+     * 它将文件上传到服务器，并返回一个可用于将文件存储在数据库中的 StorableObject
+     *
+     * @param filename 要上传的文件的名称。
+     * @param in 要上传的文件的输入流。
+     *
+     * @return 一个可存储对象。
+     */
     StorableObject uploadTmp(String filename, InputStream in);
 
+    /**
+     * 将源对象复制到目标对象
+     *
+     * @param sourceObject 源对象的名称。
+     * @param targetObject 目标对象的名称。
+     */
     void copy(String sourceObject, String targetObject);
 
+    /**
+     * 将源对象移动到目标对象。
+     *
+     * @param sourceObject 要移动的对象。
+     * @param targetObject 要将 sourceObject 移动到的目标对象。
+     */
     void move(String sourceObject, String targetObject);
 
+    /**
+     * 删除具有给定名称的对象
+     *
+     * @param objectName 要删除的对象的名称。
+     */
     void delete(String objectName);
 }
