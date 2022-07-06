b50e2fee655ebea0817787a7afdec8e17df29bc5
cn-src <public@javaer.cn>
2022-07-07 00:23:57 +0800

    refactor: update jany.execution config

--- jany-spring-boot-starter/src/main/java/cn/javaer/jany/spring/autoconfigure/task/ExecutorsProperties.java
+++ jany-spring-boot-starter/src/main/java/cn/javaer/jany/spring/autoconfigure/task/ExecutorsProperties.java
@@ -1,7 +1,6 @@
 package cn.javaer.jany.spring.autoconfigure.task;
 
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.LinkedHashMap;
@@ -10,10 +9,9 @@ import java.util.Map;
 /**
  * @author cn-src
  */
-@Getter
-@Setter
 @ConfigurationProperties(prefix = "jany.task")
 public class ExecutorsProperties {
 
-    private Map<String, ExecutionProperties> executors = new LinkedHashMap<>();
+    @Getter
+    private final Map<String, ExecutionProperties> executors = new LinkedHashMap<>();
 }
--- jany-spring-boot-starter/src/main/java/cn/javaer/jany/spring/autoconfigure/task/SchedulersProperties.java
+++ jany-spring-boot-starter/src/main/java/cn/javaer/jany/spring/autoconfigure/task/SchedulersProperties.java
@@ -1,7 +1,6 @@
 package cn.javaer.jany.spring.autoconfigure.task;
 
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.LinkedHashMap;
@@ -10,10 +9,8 @@ import java.util.Map;
 /**
  * @author cn-src
  */
-@Getter
-@Setter
 @ConfigurationProperties(prefix = "jany.scheduling")
 public class SchedulersProperties {
-
-    private Map<String, SchedulingProperties> schedulers = new LinkedHashMap<>();
+    @Getter
+    private final Map<String, SchedulingProperties> schedulers = new LinkedHashMap<>();
 }
