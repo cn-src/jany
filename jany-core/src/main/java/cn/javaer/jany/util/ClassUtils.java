666b18b98ed2ac4ec1a0b584bb2af9300e5e9696
cn-src <public@javaer.cn>
2022-07-07 11:27:11 +0800

    feat: add ClassUtils

---
+++ jany-core/src/main/java/cn/javaer/jany/util/ClassUtils.java
@@ -0,0 +1,17 @@
+package cn.javaer.jany.util;
+
+import cn.hutool.core.lang.Assert;
+import cn.hutool.core.util.ClassUtil;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
+/**
+ * @author cn-src
+ */
+public class ClassUtils extends ClassUtil {
+    public static boolean isNotStatic(Field field) {
+        Assert.notNull(field, "Field to provided is null.");
+        return !Modifier.isStatic(field.getModifiers());
+    }
+}
