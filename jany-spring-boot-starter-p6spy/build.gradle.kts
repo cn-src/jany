69b1ed363ba2a55681855ff0aaea048ba74f95d1
cn-src <public@javaer.cn>
2022-07-10 04:11:04 +0800

    refactor: p6spy

--- jany-spring-boot-starter-p6spy/build.gradle.kts
+++ jany-spring-boot-starter-p6spy/build.gradle.kts
@@ -5,7 +5,7 @@ dependencies {
     api(project(":jany-p6spy"))
 
     optionalApi("io.ebean:ebean")
-    optionalApi("com.github.gavlyukovskiy:p6spy-spring-boot-starter:$datasourceDecoratorVersion")
+    optionalApi("com.github.gavlyukovskiy:p6spy-spring-boot-starter")
     optionalApi("org.springframework.boot:spring-boot-starter")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
--- jany-spring-boot-starter-p6spy/src/main/java/cn/javaer/jany/spring/autoconfigure/p6spy/P6spyAutoConfiguration.java
+++ jany-spring-boot-starter-p6spy/src/main/java/cn/javaer/jany/spring/autoconfigure/p6spy/P6spyAutoConfiguration.java
@@ -19,16 +19,11 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration(proxyBeanMethods = false)
 @ConditionalOnClass({Loggable.class, BeautifulFormat.class, TimestampJdbcEventListener.class})
+@AutoConfigureBefore({DataSourceDecoratorAutoConfiguration.class})
 @ConditionalOnProperty(prefix = "jany.p6spy", name = "enabled", havingValue = "true",
     matchIfMissing = true)
 @EnableConfigurationProperties(P6spyProperties.class)
 public class P6spyAutoConfiguration implements InitializingBean {
-
-    @Configuration(proxyBeanMethods = false)
-    @AutoConfigureBefore({DataSourceDecoratorAutoConfiguration.class})
-    public static class OnSpringDataSourceConfiguration {
-
-    }
 
     @Override
     public void afterPropertiesSet() {
