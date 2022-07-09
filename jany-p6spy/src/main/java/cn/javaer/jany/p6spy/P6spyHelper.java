949e2695bc1528dcd0e8a109c3eb71cd63f965ee
cn-src <public@javaer.cn>
2022-07-10 06:05:38 +0800

    fix: p6spy

--- jany-p6spy/src/main/java/cn/javaer/jany/p6spy/P6spyHelper.java
+++ jany-p6spy/src/main/java/cn/javaer/jany/p6spy/P6spyHelper.java
@@ -1,11 +1,16 @@
 package cn.javaer.jany.p6spy;
 
+import com.p6spy.engine.logging.P6LogFactory;
+import com.p6spy.engine.spy.P6ModuleManager;
+import com.p6spy.engine.spy.P6SpyFactory;
 import com.p6spy.engine.spy.option.EnvironmentVariables;
 import com.p6spy.engine.spy.option.P6OptionsSource;
 import com.p6spy.engine.spy.option.SpyDotProperties;
 import com.p6spy.engine.spy.option.SystemProperties;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -38,6 +43,13 @@ public class P6spyHelper {
             .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
                 // always using value from the first P6OptionsSource
                 (value1, value2) -> value1));
+        String customModuleList = props.get("modulelist");
+        if (customModuleList == null) {
+            List<String> moduleList = new ArrayList<>();
+            moduleList.add(P6SpyFactory.class.getName());
+            moduleList.add(P6LogFactory.class.getName());
+            System.setProperty("p6spy.config.modulelist", String.join(",", moduleList));
+        }
         //noinspection AlibabaUndefineMagicConstant
         if (!props.containsKey("logMessageFormat")) {
             System.setProperty("p6spy.config.logMessageFormat",
@@ -50,6 +62,7 @@ public class P6spyHelper {
             System.setProperty("p6spy.config.appender",
                 "com.p6spy.engine.spy.appender.Slf4JLogger");
         }
+        P6ModuleManager.getInstance().reload();
         initialized = true;
     }
 
