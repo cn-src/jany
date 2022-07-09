97bc55b9dc23cdbc90e87dbad118f35554514ebf
cn-src <public@javaer.cn>
2022-07-10 03:27:03 +0800

    refactor: p6spy

---
+++ jany-p6spy/src/main/java/cn/javaer/jany/p6spy/P6spyUtils.java
@@ -0,0 +1,42 @@
+package cn.javaer.jany.p6spy;
+
+import com.p6spy.engine.spy.option.EnvironmentVariables;
+import com.p6spy.engine.spy.option.P6OptionsSource;
+import com.p6spy.engine.spy.option.SpyDotProperties;
+import com.p6spy.engine.spy.option.SystemProperties;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * @author cn-src
+ */
+public class P6spyUtils {
+    public static void initConfig() {
+        SpyDotProperties spyDotProperties = null;
+        try {
+            spyDotProperties = new SpyDotProperties();
+        }
+        catch (final IOException ignored) {
+        }
+        final Map<String, String> props = Stream.of(spyDotProperties,
+                new EnvironmentVariables(), new SystemProperties())
+            .filter(Objects::nonNull)
+            .map(P6OptionsSource::getOptions)
+            .filter(Objects::nonNull)
+            .flatMap(options -> options.entrySet().stream())
+            .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+                // always using value from the first P6OptionsSource
+                (value1, value2) -> value1));
+        //noinspection AlibabaUndefineMagicConstant
+        if (!props.containsKey("logMessageFormat")) {
+            System.setProperty("p6spy.config.logMessageFormat",
+                BeautifulFormat.class.getName());
+            System.setProperty("p6spy.config.customLogMessageFormat",
+                "time %(executionTime) ms | url %(url)\n%(sql)");
+        }
+    }
+}
