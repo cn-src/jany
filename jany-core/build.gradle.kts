a302b4c30b147fc4248f02a1cae01df971b8c6bb
cn-src <public@javaer.cn>
2022-07-11 01:13:17 +0800

    feat: add JanyVersion

--- jany-core/build.gradle.kts
+++ jany-core/build.gradle.kts
@@ -11,3 +11,26 @@ dependencies {
 
     testImplementation(project(":jany-test"))
 }
+
+file("src/main/java/cn/javaer/jany/JanyVersion.java").writeText(
+    """
+    package cn.javaer.jany;
+
+    /**
+     * 此文件自动生成。
+     *
+     * @author cn-src
+     */
+    public interface JanyVersion {
+
+        /**
+         * 返回应用程序的版本
+         *
+         * @return 程序的版本。
+         */
+        static String getVersion() {
+            return "${project.version}";
+        }
+    }
+""".trimIndent()
+)
---
+++ jany-core/src/main/java/cn/javaer/jany/JanyVersion.java
@@ -0,0 +1,18 @@
+package cn.javaer.jany;
+
+/**
+ * 此文件自动生成。
+ *
+ * @author cn-src
+ */
+public interface JanyVersion {
+
+    /**
+     * 返回应用程序的版本
+     *
+     * @return 程序的版本。
+     */
+    static String getVersion() {
+        return "latest-SNAPSHOT";
+    }
+}
