c4032cf368ad773870b455b36fcc435b3461852d
cn-src <public@javaer.cn>
2022-07-12 01:12:05 +0800

    test: new test case

---
+++ jany-ebean/src/test/java/cn/javaer/jany/ebean/EbeanOptChainTest.java
@@ -0,0 +1,21 @@
+package cn.javaer.jany.ebean;
+
+import cn.javaer.jany.ebean.query.QDemo;
+import cn.javaer.jany.util.OptChain;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author cn-src
+ */
+public class EbeanOptChainTest {
+    @Test
+    void testQBean() {
+        OptChain.of(new QDemo())
+            .opt(q -> q.id::eq, 1)
+            .opt(q -> q.id::between, 1, 4)
+            .call(QDemo::or)
+            .opt(q -> q.name::eq, "name")
+            .call(QDemo::endOr)
+            .root().findList();
+    }
+}
