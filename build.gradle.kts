20188394ffe40116d8c30984df6274a1e8d6084e
cn-src <public@javaer.cn>
2022-08-29 19:45:42 +0800

    build: update to latest-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 subprojects {
     apply(plugin = "maven-publish")
     group = "cn.javaer.jany"
-    version = "0.1.0"
+    version = "latest-SNAPSHOT"
 
     repositories {
         mavenCentral()
