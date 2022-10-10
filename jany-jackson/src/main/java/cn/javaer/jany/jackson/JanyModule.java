aa1ce2c6dee9edafda18a3541c1707029664a62e
cn-src <public@javaer.cn>
2022-10-11 01:43:13 +0800

    feat: JanyModule

--- jany-jackson/src/main/java/cn/javaer/jany/jackson/JanyModule.java
+++ jany-jackson/src/main/java/cn/javaer/jany/jackson/JanyModule.java
@@ -1,9 +1,34 @@
 package cn.javaer.jany.jackson;
 
+import cn.javaer.jany.model.KeyValue;
+import cn.javaer.jany.util.TimeUtils;
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public final class JanyModule extends SimpleModule {
-   
+    public JanyModule() {
+        // @formatter:off
+        addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(TimeUtils.DATE_TIME_FORMATTER));
+        addSerializer(LocalDate.class, new LocalDateSerializer(TimeUtils.DATE_FORMATTER));
+        addSerializer(LocalTime.class, new LocalTimeSerializer(TimeUtils.TIME_FORMATTER));
+        addSerializer(KeyValue.class, KeyValueSerializer.INSTANCE);
+
+        addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(TimeUtils.DATE_TIME_FORMATTER));
+        addDeserializer(LocalDate.class, new LocalDateDeserializer(TimeUtils.DATE_FORMATTER));
+        addDeserializer(LocalTime.class, new LocalTimeDeserializer(TimeUtils.TIME_FORMATTER));
+        addDeserializer(KeyValue.class, KeyValueDeserializer.INSTANCE);
+        // @formatter:on
+    }
+
     @Override
     public void setupModule(SetupContext context) {
         context.appendAnnotationIntrospector(JanyJacksonAnnotationIntrospector.INSTANCE);
