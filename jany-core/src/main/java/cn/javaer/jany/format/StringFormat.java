d2b0d82fae60eac752dbc375fdc5ffeea1bfce93
cn-src <public@javaer.cn>
2022-10-11 13:51:56 +0800

    feat: StringFormat

---
+++ jany-core/src/main/java/cn/javaer/jany/format/StringFormat.java
@@ -0,0 +1,19 @@
+package cn.javaer.jany.format;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author cn-src
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.PARAMETER, ElementType.ANNOTATION_TYPE})
+public @interface StringFormat {
+    boolean apply() default true;
+
+    boolean emptyToNull() default true;
+}
