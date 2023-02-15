01f2d587f05b40edf446218db8e18502e3c50554
cn-src <public@javaer.cn>
2023-02-15 14:28:37 +0800

    build: 释放 0.3.0 版本

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 subprojects {
     apply(plugin = "maven-publish")
     group = "cn.javaer.jany"
-    version = "latest-SNAPSHOT"
+    version = "0.3.0"
 
     repositories {
         mavenCentral()
