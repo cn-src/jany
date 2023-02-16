6ac7b93ba6b41e994484b0157594e7d81b0de4db
cn-src <public@javaer.cn>
2023-02-16 11:32:29 +0800

    fix: 修复错误消息不存在的异常

--- jany-spring/src/main/java/cn/javaer/jany/spring/web/exception/ErrorMessageSource.java
+++ jany-spring/src/main/java/cn/javaer/jany/spring/web/exception/ErrorMessageSource.java
@@ -55,14 +55,19 @@ public class ErrorMessageSource extends ResourceBundleMessageSource {
     }
 
     public static String getMessage(final String error, final Object[] args) {
-        return ACCESSOR.getMessage(error, args);
+        try {
+            return ACCESSOR.getMessage(error, args);
+        }
+        catch (NoSuchMessageException e) {
+            return "";
+        }
     }
 
     public static String getMessage(final ErrorInfo errorInfo, final Throwable t) {
         if (EL_KEY_START != errorInfo.getError().charAt(0)) {
             return getMessage(errorInfo);
         }
-        final String messageEl = ACCESSOR.getMessage(errorInfo.getError());
+        final String messageEl = getMessage(errorInfo.getError());
         final EvaluationContext context = new StandardEvaluationContext();
         context.setVariable("e", t);
         final String value = elParser.parseExpression(messageEl).getValue(context, String.class);
