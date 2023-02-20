35544f8400e2d542593ecee5fb63624ff059efc0
cn-src <public@javaer.cn>
2023-02-20 15:45:16 +0800

    chore: clean

--- jany-spring-boot-starter/src/main/java/cn/javaer/jany/spring/autoconfigure/jackson/JacksonPlusAutoConfiguration.java
+++ jany-spring-boot-starter/src/main/java/cn/javaer/jany/spring/autoconfigure/jackson/JacksonPlusAutoConfiguration.java
@@ -5,9 +5,6 @@ import cn.javaer.jany.jackson.JooqJsonbDeserializer;
 import cn.javaer.jany.jackson.JooqJsonbSerializer;
 import cn.javaer.jany.jackson.JooqRecordSerializer;
 import cn.javaer.jany.jackson.Json;
-import cn.javaer.jany.jackson.KeyValueDeserializer;
-import cn.javaer.jany.jackson.KeyValueSerializer;
-import cn.javaer.jany.model.KeyValue;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
@@ -63,8 +60,6 @@ public class JacksonPlusAutoConfiguration {
                 new LocalDateDeserializer(dateFormatter));
             it.deserializerByType(LocalTime.class,
                 new LocalTimeDeserializer(timeFormatter));
-            it.deserializerByType(KeyValue.class,
-                KeyValueDeserializer.INSTANCE);
 
             it.serializerByType(LocalDateTime.class,
                 new LocalDateTimeSerializer(dateTimeFormatter));
@@ -72,8 +67,6 @@ public class JacksonPlusAutoConfiguration {
                 new LocalDateSerializer(dateFormatter));
             it.serializerByType(LocalTime.class,
                 new LocalTimeSerializer(timeFormatter));
-            it.serializerByType(KeyValue.class,
-                KeyValueSerializer.INSTANCE);
         };
     }
 
