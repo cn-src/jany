2ff72133eba819cfc3c29d57e354538b688312bc
cn-src <public@javaer.cn>
2023-02-20 15:36:39 +0800

    chore: update javadoc

--- jany-archunit/src/main/java/cn/javaer/jany/archunit/SubFields.java
+++ jany-archunit/src/main/java/cn/javaer/jany/archunit/SubFields.java
@@ -9,7 +9,7 @@ import java.lang.annotation.Target;
 /**
  * 检测被注解 {@link SubFields} 的类的字段是否是 {@link SubFields#value()} 声明的类的字段的子集.
  * <p>
- * 判断时忽略静态字段和被 {@link org.springframework.data.annotation.Transient} 标注的字段.
+ * 判断时忽略静态字段和被 {@code org.springframework.data.annotation.Transient} 标注的字段.
  * </p>
  *
  * @author cn-src
