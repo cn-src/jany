c2dde17e52d002f12eca4812f88f401a242e7183
cn-src <public@javaer.cn>
2023-03-13 16:50:25 +0800

    build: 发布 0.4.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 subprojects {
     apply(plugin = "maven-publish")
     group = "cn.javaer.jany"
-    version = "latest-SNAPSHOT"
+    version = "0.4.0"
 
     repositories {
         mavenCentral()
@@ -13,10 +13,20 @@ subprojects {
                 if (!project.hasProperty("signing.keyId")) {
                     version = "latest-SNAPSHOT"
                 }
-                url = uri(System.getenv("publicSnapshotsRepoUrl"))
-                credentials {
-                    username = System.getenv("publicRepoUsername")
-                    password = System.getenv("publicRepoPassword")
+                val releasesRepoUrl = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2")
+                val snapshotsRepoUrl = uri("https://packages.aliyun.com/maven/repository/2163442-snapshot-zWHJ5H/")
+                if (version.toString().endsWith("SNAPSHOT")) {
+                    url = snapshotsRepoUrl
+                    credentials {
+                        username = providers.gradleProperty("aliyunRepoUsername").getOrElse("")
+                        password = providers.gradleProperty("aliyunRepoPassword").getOrElse("")
+                    }
+                } else {
+                    url = releasesRepoUrl
+                    credentials {
+                        username = providers.gradleProperty("ossrhRepoUsername").getOrElse("")
+                        password = providers.gradleProperty("ossrhRepoPassword").getOrElse("")
+                    }
                 }
             }
         }
--- jany-core/src/main/java/cn/javaer/jany/JanyVersion.java
+++ jany-core/src/main/java/cn/javaer/jany/JanyVersion.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2020-2023 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *       https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package cn.javaer.jany;
 
 /**
@@ -29,6 +13,6 @@ public interface JanyVersion {
      * @return 程序的版本。
      */
     static String getVersion() {
-        return "latest-SNAPSHOT";
+        return "0.4.0";
     }
 }
