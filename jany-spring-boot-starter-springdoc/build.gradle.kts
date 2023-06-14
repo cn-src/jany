1c2bb9a0c979e2c2b427633d6d19b9c645258df2
cn-src <public@javaer.cn>
2023-06-14 18:13:27 +0800

    feat: 支持 jdk17 和 springboot 3

--- jany-spring-boot-starter-springdoc/build.gradle.kts
+++ jany-spring-boot-starter-springdoc/build.gradle.kts
@@ -4,9 +4,9 @@ plugins {
 val springdocVersion: String by project
 dependencies {
     api(project(":jany-spring-boot-starter"))
-    api("org.springdoc:springdoc-openapi-webmvc-core:$springdocVersion")
+    api("org.springdoc:springdoc-openapi-starter-webmvc-api:$springdocVersion")
 
-    optionalApi("org.springdoc:springdoc-openapi-data-rest:$springdocVersion")
+//    optionalApi("org.springdoc:springdoc-openapi-data-rest:$springdocVersion")
     optionalApi("org.springframework.boot:spring-boot-starter-data-jdbc")
     testImplementation("org.springframework.boot:spring-boot-starter-web")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
