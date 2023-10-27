6965318f9954e26de82499b8edfc7fd28e1379bf
cn-src <public@javaer.cn>
2023-10-27 10:31:41 +0800

    build: 移除部分模块

--- jany-platform/build.gradle.kts
+++ jany-platform/build.gradle.kts
@@ -14,7 +14,7 @@ dependencies {
     api(platform("com.playtika.testcontainers:testcontainers-spring-boot-bom:${p["playtikaVersion"]}"))
 
     constraints {
-        api(project(":jany-archunit"))
+//        api(project(":jany-archunit"))
         api(project(":jany-core"))
         api(project(":jany-ebean"))
         api(project(":jany-jackson"))
@@ -23,8 +23,8 @@ dependencies {
         api(project(":jany-p6spy"))
         api(project(":jany-spring"))
         api(project(":jany-spring-boot-starter"))
-        api(project(":jany-spring-boot-starter-handlebars"))
-        api(project(":jany-spring-boot-starter-minio"))
+//        api(project(":jany-spring-boot-starter-handlebars"))
+//        api(project(":jany-spring-boot-starter-minio"))
         api(project(":jany-spring-boot-starter-p6spy"))
         api(project(":jany-spring-boot-starter-springdoc"))
         api(project(":jany-test"))
