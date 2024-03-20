416b465a5e9928969d7bc293790d2509fdbc3e62
cn-src <public@javaer.cn>
2024-03-20 10:32:24 +0800

    chore: clean code

--- jany-archunit/build.gradle.kts
+++
@@ -1,8 +0,0 @@
-plugins {
-    id("jany-library")
-}
-val archunitVersion: String by project
-
-dependencies {
-    api("com.tngtech.archunit:archunit-junit5:$archunitVersion")
-}
--- jany-config/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-plugins {
-    id("jany-library")
-}
-val redissonVersion: String by project
-dependencies {
-    api("org.redisson:redisson-spring-boot-starter:$redissonVersion")
-
-    testImplementation("com.redis.testcontainers:testcontainers-redis-junit:1.6.4")
-}
