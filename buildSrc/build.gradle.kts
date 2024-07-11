da1a2dd41d9997f4dea3793ad7029df51b4571d1
cn-src <public@javaer.cn>
2024-07-11 13:10:41 +0800

    build: 更新依赖

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 dependencies {
-    implementation("io.spring.gradle:dependency-management-plugin:1.1.5")
+    implementation("io.spring.gradle:dependency-management-plugin:1.1.6")
 }
 
 repositories {
