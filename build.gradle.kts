a5381047e34487d4d2ca0bec8aa82fcd7cd994af
cn-src <public@javaer.cn>
2024-10-29 13:18:26 +0800

    build: 释放 1.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ subprojects {
     val snapshotVersion = "next-SNAPSHOT"
     apply(plugin = "maven-publish")
     group = "cn.javaer.jany"
-    version = snapshotVersion
+    version = "1.3.0"
 
     repositories {
         mavenCentral()
